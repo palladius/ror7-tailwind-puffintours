94b10ab61880294bf94243663047901d338d21f4
Riccardo Carlesso <palladiusbonton@gmail.com>
2023-10-14 19:25:50 +0200

    0.1.0c restored localhost

--- config/initializers/riccardo.rb
+++ config/initializers/riccardo.rb
@@ -8,7 +8,8 @@ AppEmoji = '🐧🧳'
 # TODO move to ENV :)
 #Rails.application.config.hosts << "dhh-vanilla-701-dev-cdlu26pd4q-oa.a.run.app"
 Rails.application.config.hosts << 'puffintours-prova-con-secret-rjjr63dzrq-uc.a.run.app'
-#Rails.application.config.hosts << "localhost"
+# Se no non va in localhost :)
+Rails.application.config.hosts << "localhost"
 
 # https://github.com/mbleigh/acts-as-taggable-on?search=1
 ActsAsTaggableOn.force_lowercase = true
