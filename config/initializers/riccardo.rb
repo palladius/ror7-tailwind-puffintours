d0f8b6a449f01901c3602e32ffef8dca345aa8b6
Riccardo Carlesso <palladiusbonton@gmail.com>
2023-10-22 18:10:23 +0200

    0.1.4b fixing unknown vars

--- config/initializers/riccardo.rb
+++ config/initializers/riccardo.rb
@@ -9,8 +9,10 @@ AppEmoji = '🐧🧳'
 #Rails.application.config.hosts << "dhh-vanilla-701-dev-cdlu26pd4q-oa.a.run.app"
 Rails.application.config.hosts << 'puffintours-prova-con-secret-rjjr63dzrq-uc.a.run.app'
 Rails.application.config.hosts << 'puffintours-prod-rjjr63dzrq-ew.a.run.app'
+#Rails.application.config.hosts << 'puffintours-prod-rjjr63dzrq-ew.a.run.app'
+
 # TODO Add an ENV cosi glielo do da ENV e vaffanbagno!
-Rails.application.config.hosts << ENV['APP_CLOUDRUN_URL'] if  ENV.fetch('APP_CLOUDRUN_URL') # rescue :boh
+Rails.application.config.hosts << ENV['APP_CLOUDRUN_URL'] if ENV.fetch('APP_CLOUDRUN_URL') # rescue :boh
 
 # Se no non va in localhost :)
 #E, [2023-10-22T10:06:40.314767 #30] ERROR -- : [ActionDispatch::HostAuthorization::DefaultResponseApp] Blocked host: puffintours-prod-rjjr63dzrq-ew.a.run.app
