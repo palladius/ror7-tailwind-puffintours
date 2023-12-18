eebace74bb19294387f0dbdef7338e794a2a9a82
Riccardo Carlesso <palladiusbonton@gmail.com>
2023-12-18 21:17:10 +0100

    0.4.01 minor changes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
+# Idea and implementation from here: https://dev.to/jamgar/how-to-create-a-comment-and-reply-system-in-ruby-on-rails-10ld
 class Comment < ApplicationRecord
   belongs_to :article
   belongs_to :user
