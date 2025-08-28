aaf287dfc72f4d387da693848dc1023c066e2849
Riccardo Carlesso <ricc+chezmoi@google.com>
2025-08-28 11:08:54 +0200

    feat: Add family page
    This commit introduces a new family page that displays cards for each family member. The data is loaded from a YAML file.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,11 @@
 class PagesController < ApplicationController
   include GcpHelper
-  before_action :authenticate_user!, only: [:family]
-
-  def family
-  end
+  before_action :authenticate_user!, only: [:show], if: -> { params[:page] == 'family' }
 
   def show
+    if params[:page] == 'family'
+      @family_members = YAML.safe_load(File.read(Rails.root.join('config', 'family.yml')), permitted_classes: [Date])
+    end
     render template: "pages/#{params[:page]}"
       rescue ActionView::MissingTemplate
     render "errors/not_found", status: 404
