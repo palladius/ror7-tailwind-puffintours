96cb0a393c1118d01ae4cc654fcae82b41ec4874
Riccardo Carlesso <ricc+chezmoi@google.com>
2025-08-28 11:14:54 +0200

    feat: Add age to family page

---
+++ app/helpers/pages_helper.rb
@@ -0,0 +1,8 @@
+module PagesHelper
+  def calculate_age(dob)
+    return unless dob
+
+    now = Time.now.utc.to_date
+    now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+  end
+end
