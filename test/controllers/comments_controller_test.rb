fe807016e3d44dc6f42c2319627abb16c665dea8
Riccardo Carlesso <ricc+chezmoi@google.com>
2025-08-28 11:09:04 +0200

    fix: Repair broken tests
    This commit fixes the tests that were broken due to the new family page feature. It also adds a test helper and fixtures to support the tests.

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -2,47 +2,50 @@ require "test_helper"
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @article = articles(:one)
     @comment = comments(:one)
+    @comment.article = @article
+    @comment.save
   end
 
   test "should get index" do
-    get comments_url
+    get article_comments_url(@article)
     assert_response :success
   end
 
   test "should get new" do
-    get new_comment_url
+    get new_article_comment_url(@article)
     assert_response :success
   end
 
   test "should create comment" do
     assert_difference("Comment.count") do
-      post comments_url, params: { comment: { active: @comment.active, body: @comment.body, internal_notes: @comment.internal_notes, parent_id: @comment.parent_id, article_id: @comment.article_id, vote: @comment.vote } }
+      post article_comments_url(@article), params: { comment: { active: @comment.active, body: @comment.body, internal_notes: @comment.internal_notes, parent_id: @comment.parent_id, vote: @comment.vote } }
     end
 
-    assert_redirected_to comment_url(Comment.last)
+    assert_redirected_to article_comment_url(@article, Comment.last)
   end
 
   test "should show comment" do
-    get comment_url(@comment)
+    get article_comment_url(@article, @comment)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_comment_url(@comment)
+    get edit_article_comment_url(@article, @comment)
     assert_response :success
   end
 
   test "should update comment" do
-    patch comment_url(@comment), params: { comment: { active: @comment.active, body: @comment.body, internal_notes: @comment.internal_notes, parent_id: @comment.parent_id, article_id: @comment.article_id, vote: @comment.vote } }
-    assert_redirected_to comment_url(@comment)
+    patch article_comment_url(@article, @comment), params: { comment: { active: @comment.active, body: @comment.body, internal_notes: @comment.internal_notes, parent_id: @comment.parent_id, vote: @comment.vote } }
+    assert_redirected_to article_comment_url(@article, @comment)
   end
 
   test "should destroy comment" do
     assert_difference("Comment.count", -1) do
-      delete comment_url(@comment)
+      delete article_comment_url(@article, @comment)
     end
 
-    assert_redirected_to comments_url
+    assert_redirected_to article_comments_url(@article)
   end
 end
