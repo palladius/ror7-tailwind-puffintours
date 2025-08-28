740356df6869c80cdb1000b3f774583356ed1bd3
Riccardo Carlesso <ricc+chezmoi@google.com>
2025-08-28 11:11:53 +0200

    fix: Fix broken family page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,12 @@
 class PagesController < ApplicationController
   include GcpHelper
-  before_action :authenticate_user!, only: [:show], if: -> { params[:page] == 'family' }
+  before_action :authenticate_user!, only: [:family]
+
+  def family
+    @family_members = YAML.safe_load(File.read(Rails.root.join('config', 'family.yml')), permitted_classes: [Date])
+  end
 
   def show
-    if params[:page] == 'family'
-      @family_members = YAML.safe_load(File.read(Rails.root.join('config', 'family.yml')), permitted_classes: [Date])
-    end
     render template: "pages/#{params[:page]}"
       rescue ActionView::MissingTemplate
     render "errors/not_found", status: 404
